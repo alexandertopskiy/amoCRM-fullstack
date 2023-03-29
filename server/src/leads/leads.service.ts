1df36c145a4bb22d3faac5becdc4bacaa5f258ec
Alexander Loshakov <alevas556@icloud.com>
2023-03-29 09:20:17 +0300

    Запрос на получение сделок

--- server/src/leads/leads.service.ts
+++ server/src/leads/leads.service.ts
@@ -1,10 +1,24 @@
+import axios from 'axios';
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class LeadsService {
-    getLeads(query: string): string {
-        const res = 'get all leads with query: ' + query;
-        console.log(res);
-        return res;
+    async getLeads(query: string) {
+        try {
+            const res = 'get all leads with query: ' + query;
+            console.log(res);
+
+            const url = `https://${process.env.SUBDOMAIN}.amocrm.ru/api/v4/leads`;
+            const response = await axios.get(url);
+            console.log('\nSUCCESS: ' + response + '\n\n\n');
+
+            return res;
+        } catch (error) {
+            const code = error.response.status;
+            // 401 - Unauthorized
+            if (code === 401) {
+                console.log('Unauthorized. Try to Auth');
+            }
+        }
     }
 }
--- server/src/main.ts
+++ server/src/main.ts
@@ -1,5 +1,9 @@
+import * as dotenv from 'dotenv';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+
+// загрузка содержимого .env в process.env
+dotenv.config();
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
