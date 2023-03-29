5687c2ae3858eb655e95968a09e24694cf44d8c8
Alexander Loshakov <alevas556@icloud.com>
2023-03-29 18:14:03 +0300

    Чаастичный вывод таблицы с mock-данными

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,17 +1,24 @@
 <template>
-    <a-layout style="height: 100%">
-        <a-layout-content style="padding: 0 50px; height: 100%">
-            <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+    <a-layout class="layout">
+        <a-layout-content class="layout-content">
+            <LeadsContent />
         </a-layout-content>
     </a-layout>
 </template>
 
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue';
+import LeadsContent from './components/LeadsContent.vue';
 </script>
 
 <style lang="scss">
 #app {
+    height: 100%;
+}
+.layout {
+    height: 100%;
+}
+.layout-content {
+    padding: 50px;
     height: 100%;
 }
 </style>
---
+++ client/src/components/LeadsContent.vue
@@ -0,0 +1,63 @@
+<template>
+    <a-card title="Список всех сделок" :bordered="false">
+        <template #extra>
+            <input type="text" placeholder="Поиск сделок" />
+        </template>
+        <a-table :dataSource="dataSource" :columns="columns" :pagination="false" />
+    </a-card>
+</template>
+
+<script setup lang="ts">
+const dataSource = [
+    {
+        'id': 1804003,
+        'name': 'Продажа мешка картошки',
+        'price': 100,
+        'responsible_user': {
+            'id': 68,
+            'name': 'тест',
+            'email': 'test@test.test'
+        },
+        'status': {
+            'id': 56277310,
+            'name': 'Согласование договора',
+            'color': '#ffcccc'
+        },
+        'created_at': 1680077630,
+        'contacts': [
+            {
+                'id': 3333799,
+                'name': 'Михаил'
+            }
+        ]
+    }
+];
+
+const columns = [
+    {
+        title: 'Название',
+        dataIndex: 'name',
+        key: 'name'
+    },
+    {
+        title: 'Бюджет',
+        dataIndex: 'price',
+        key: 'price'
+    },
+    {
+        title: 'Ответственный',
+        dataIndex: ['responsible_user', 'name'],
+        key: 'responsible_user'
+    },
+    {
+        title: 'Статус',
+        dataIndex: ['status', 'name'],
+        key: 'status'
+    },
+    {
+        title: 'Дата создания',
+        dataIndex: 'created_at',
+        key: 'created_at'
+    }
+];
+</script>
