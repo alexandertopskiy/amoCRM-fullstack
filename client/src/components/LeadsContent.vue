686c5bdaff4e1f21a12908fee7642606469a27b0
Alexander Loshakov <alevas556@icloud.com>
2023-03-30 10:23:07 +0300

    Получение данных из стора

--- client/src/components/LeadsContent.vue
+++ client/src/components/LeadsContent.vue
@@ -37,53 +37,16 @@
 </template>
 
 <script setup lang="ts">
+import { computed } from 'vue';
+import { useStore } from 'vuex';
 import moment from 'moment';
 import { UserOutlined } from '@ant-design/icons-vue';
 import SearchBar from './SearchBar.vue';
 
+const store = useStore();
 moment.locale('ru');
 
-const dataSource = [
-    {
-        'id': 1804003,
-        'name': 'Продажа мешка картошки',
-        'price': 100,
-        'responsible_user': {
-            'id': 68,
-            'name': 'тест',
-            'email': 'test@test.test'
-        },
-        'status': {
-            'id': 56277310,
-            'name': 'Согласование договора',
-            'color': '#ffcccc'
-        },
-        'created_at': '2023-03-29T08:13:50.000Z',
-        'contacts': [
-            {
-                'id': 3333799,
-                'name': 'Михаил'
-            }
-        ]
-    },
-    {
-        'id': 1860343,
-        'name': 'Чебупели по акции',
-        'price': 149,
-        'responsible_user': {
-            'id': 9428602,
-            'name': 'alexander',
-            'email': 'dai_digi_dig_dai@mail.ru'
-        },
-        'status': {
-            'id': 56277298,
-            'name': 'Первичный контакт',
-            'color': '#99ccff'
-        },
-        'created_at': '2023-03-29T16:22:59.000Z',
-        'contacts': []
-    }
-];
+const dataSource = computed(() => store.getters.leads);
 
 const columns = [
     {
--- client/src/store/index.ts
+++ client/src/store/index.ts
@@ -1,8 +1,64 @@
 import { createStore } from 'vuex';
 
 export default createStore({
-    state: {},
-    getters: {},
+    state: {
+        isLoading: false,
+        searchQuery: '',
+        leads: [
+            {
+                'id': 1804003,
+                'name': 'Продажа мешка картошки',
+                'price': 100,
+                'responsible_user': {
+                    'id': 68,
+                    'name': 'тест',
+                    'email': 'test@test.test'
+                },
+                'status': {
+                    'id': 56277310,
+                    'name': 'Согласование договора',
+                    'color': '#ffcccc'
+                },
+                'created_at': '2023-03-29T08:13:50.000Z',
+                'contacts': [
+                    {
+                        'id': 3333799,
+                        'name': 'Михаил',
+                        'phone': '333777',
+                        'email': 'alex@test.com'
+                    }
+                ]
+            },
+            {
+                'id': 1860343,
+                'name': 'Чебупели по акции',
+                'price': 149,
+                'responsible_user': {
+                    'id': 9428602,
+                    'name': 'alexander',
+                    'email': 'dai_digi_dig_dai@mail.ru'
+                },
+                'status': {
+                    'id': 56277298,
+                    'name': 'Первичный контакт',
+                    'color': '#99ccff'
+                },
+                'created_at': '2023-03-29T16:22:59.000Z',
+                'contacts': []
+            }
+        ]
+    },
+    getters: {
+        isLoading(state) {
+            return state.isLoading;
+        },
+        searchQuery(state) {
+            return state.searchQuery;
+        },
+        leads(state) {
+            return state.leads;
+        }
+    },
     mutations: {},
     actions: {},
     modules: {}
