3d32b4650f781f5669cb602459c29cb13352aaf5
Alexander Loshakov <alevas556@icloud.com>
2023-03-30 09:24:11 +0300

    Изменение отображения Ответственного и Статуса

--- client/src/components/LeadsContent.vue
+++ client/src/components/LeadsContent.vue
@@ -3,12 +3,32 @@
         <template #extra>
             <SearchBar />
         </template>
-        <a-table :dataSource="dataSource" :columns="columns" :pagination="false" />
+        <a-table :dataSource="dataSource" :columns="columns" :pagination="false" rowKey="id">
+            <template #bodyCell="{ column, record }">
+                <!-- Ответственный -->
+                <template v-if="column.key === 'responsible_user'">
+                    <div class="res-user">
+                        <a-avatar class="res-user-icon" size="small">
+                            <template #icon><UserOutlined /></template>
+                        </a-avatar>
+                        {{ record.responsible_user.name }}
+                    </div>
+                </template>
+                <!-- Статус -->
+                <template v-if="column.key === 'status'">
+                    <a-tag :color="record.status.color" style="color: rgba(0, 0, 0, 0.65)">{{
+                        record.status.name
+                    }}</a-tag>
+                </template>
+            </template>
+        </a-table>
     </a-card>
 </template>
 
 <script setup lang="ts">
+import { UserOutlined } from '@ant-design/icons-vue';
 import SearchBar from './SearchBar.vue';
+
 const dataSource = [
     {
         'id': 1804003,
@@ -24,13 +44,30 @@ const dataSource = [
             'name': 'Согласование договора',
             'color': '#ffcccc'
         },
-        'created_at': 1680077630,
+        'created_at': '2023-03-29T08:13:50.000Z',
         'contacts': [
             {
                 'id': 3333799,
                 'name': 'Михаил'
             }
         ]
+    },
+    {
+        'id': 1860343,
+        'name': 'Чебупели по акции',
+        'price': 149,
+        'responsible_user': {
+            'id': 9428602,
+            'name': 'alexander',
+            'email': 'dai_digi_dig_dai@mail.ru'
+        },
+        'status': {
+            'id': 56277298,
+            'name': 'Первичный контакт',
+            'color': '#99ccff'
+        },
+        'created_at': '2023-03-29T16:22:59.000Z',
+        'contacts': []
     }
 ];
 
@@ -41,9 +78,9 @@ const columns = [
         key: 'name'
     },
     {
-        title: 'Бюджет',
-        dataIndex: 'price',
-        key: 'price'
+        title: 'Статус',
+        dataIndex: ['status', 'name'],
+        key: 'status'
     },
     {
         title: 'Ответственный',
@@ -51,14 +88,25 @@ const columns = [
         key: 'responsible_user'
     },
     {
-        title: 'Статус',
-        dataIndex: ['status', 'name'],
-        key: 'status'
-    },
-    {
         title: 'Дата создания',
         dataIndex: 'created_at',
         key: 'created_at'
+    },
+    {
+        title: 'Бюджет (руб)',
+        dataIndex: 'price',
+        key: 'price'
     }
 ];
 </script>
+
+<style scoped lang="scss">
+.res-user {
+    display: flex;
+    align-items: center;
+
+    &-icon {
+        margin-right: 5px;
+    }
+}
+</style>
